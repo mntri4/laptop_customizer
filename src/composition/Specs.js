56292bd97d710cec2ab81749423b67bebda45ed9
mntri4 <31458021+mntri4@users.noreply.github.com>
2020-01-05 20:51:31 -0600

    Add files via upload
    Initial load

---
+++ src/composition/Specs.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Parts from './Parts'
+import './Specs.css'
+
+
+function Specs (props) {
+    return (
+        <section className="main__form">
+            <div className="feature__name">{props.title}</div>
+            <ul className="feature__list">
+                {props.options && props.options.map((key, index, options,) =>
+                    <Parts
+                        key={index}
+                        title={props.title}
+                        parts={options[index]}
+                        selected={props.selected}
+                        name={key.name}
+                        cost={key.cost}
+                        onClick={props.onClick}
+                        bgColor={props.bgColor}
+                        change={props.change}                   
+                    />
+                )}
+            </ul>
+        </section>
+    );
+}
+
+export default Specs;
